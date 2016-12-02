dd38138d048d104acb6bc18c0c7f8fc49a2c3231
gofaster-git <rakesh.bokinala@gmail.com>
2016-12-02 17:49:10 +0000

    PORT
    Updated Listen Port

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//set environment variables
+app.set('port', process.env.PORT || 3000 );
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -57,5 +60,9 @@ app.use(function(err, req, res, next) {
   });
 });
 
+//running server on node
+var server = app.listen(app.get('port'), function () {
+	console.log('Listening on port ' + app.get('port'));
+});
 
 module.exports = app;

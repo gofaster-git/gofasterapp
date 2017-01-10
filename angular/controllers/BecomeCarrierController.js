3c117aec32cf38ed55c9bb6b840a18026ef46e88
manikumar <manikumar.repuri@gmail.com>
2017-01-10 16:05:28 +0000

    upload localhost changed to server name

--- angular/controllers/BecomeCarrierController.js
+++ angular/controllers/BecomeCarrierController.js
@@ -493,7 +493,7 @@ $scope.uploadPassport = function(file, errFiles, index){
         $scope.passportList[index].passport=file;
         if (file) {
             file.upload = Upload.upload({
-                url: '/upload',
+                url: 'http://ec2-35-167-79-162.us-west-2.compute.amazonaws.com:80/upload',
                 data: {file: file}
             });
 
